98fddbcec17f01e0b71203b920202b6889072ab9
jay vyas <jayunit100.github@gmail.com>
2019-03-06 09:41:51 -0800

    Update tmux-pdshifier.sh

--- tmux-pdshifier.sh
+++ tmux-pdshifier.sh
@@ -8,13 +8,14 @@ user=""
 [ -f ./hosts ] && echo "Hosts file found: `cat hosts`" || echo "No 'hosts' file, make a pdsh hosts file and retry.'"
 
 echo "Checks passed, starting tmux session for this cluster !"
+tmux kill-session -t jayv-tmux-hosts #-/Users/jayv/work/clusters/ec2-kube-0
 
 #### Now create sessions
 
 set -e
 set -x
 
-SESSION=${USER}-tmux-hosts-`pwd`
+SESSION=${USER}-tmux-hosts
 echo "Creating new tmux session"
 tmux -2 new-session -d -s $SESSION
 
@@ -32,7 +33,14 @@ for i in `cat ./hosts` ; do
   num=$((curr_window - 1))
   tmux list-panes -t $SESSION
   tmux select-pane -t $SESSION:.$num
-  tmux send-keys -t $SESSION:.$num "ssh-add ; ssh -v ${user}@${i}" C-m
+
+  # normally read user from the above.
+  ssh_target="${user}@${i}"
+  # if the pdsh file has format x@y, then user is hopefully empty.
+  if [[ $user == "" ]] ; then
+     ssh_target="${i}"
+  fi
+  tmux send-keys -t $SESSION:.$num "ssh-add ; ssh -v $ssh_target" C-m
   let "curr_window=curr_window+1"
 done
 
